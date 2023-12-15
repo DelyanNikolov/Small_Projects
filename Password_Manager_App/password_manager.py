e0d8c205eb1ca41486ae35bb514b3b6c66eeb1cc
DelyanNikolov <145500850+DelyanNikolov@users.noreply.github.com>
2023-12-15 21:07:53 +0200

    Replaced data.txt with json file, added some error handling, added search function

--- Password_Manager_App/password_manager.py
+++ Password_Manager_App/password_manager.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -30,16 +31,51 @@ def save():
     website = website_entry.get()
     user = username_entry.get()
     pas = password_entry.get()
+    new_data = {
+        website: {
+            "email": user,
+            "password": pas
+        }
+    }
     if len(website) == 0 or len(pas) == 0:
         messagebox.showinfo(title="Oops!", message="Please make sure all fields aren't empty")
     else:
         messagebox.askokcancel(title=f"{website}", message=f"These are the details entered: \n \nEmail: {user}"
                                                            f"\nPassword: {pas} \nIs OK to save?")
+        try:
+            with open("data.json", "r") as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            with open("data.json", "w") as file:
+                json.dump(new_data, file, indent=4)
+        else:
+            data.update(new_data)
+            with open("data.json", "w") as file:
+                json.dump(data, file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
-        with open("data.txt", "a") as file:
-            file.write(f"{website}: {user}, {pas}\n")
-        website_entry.delete(0, END)
-        password_entry.delete(0, END)
+
+# ---------------------------- SEARCH ------------------------------- #
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Oops!", message="Data file not found!")
+    else:
+        if not website:
+            # Error msg for empty field.
+            messagebox.showinfo(title=f"Oops", message=f"Website empty!")
+        elif website in data:
+            # Message with stored credentials
+            messagebox.showinfo(title=f"{website}", message=f"Username: {data[website]['email']}\n"
+                                                            f"Password: {data[website]['password']}")
+        else:
+            # Error msg when website not in stored data.
+            messagebox.showinfo(title=f"{website}", message=f"You don't have registration on this website")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -63,22 +99,25 @@ password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 
 # Entry:
-website_entry = Entry(width=52)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width=36)
+website_entry.grid(column=1, row=1)
 website_entry.focus()
 
-username_entry = Entry(width=52)
+username_entry = Entry(width=54)
 username_entry.grid(column=1, row=2, columnspan=2)
 username_entry.insert(0, "delian.nikolov@abv.bg")
 
-password_entry = Entry(width=33)
+password_entry = Entry(width=36)
 password_entry.grid(column=1, row=3)
 
 # Buttons:
+search_button = Button(text="Search", command=find_password, width=14)
+search_button.grid(column=2, row=1)
+
 generate_button = Button(text="Generate password", command=generate_password)
 generate_button.grid(column=2, row=3)
 
-add_button = Button(text="Add", width=44, command=save)
+add_button = Button(text="Add", width=46, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
